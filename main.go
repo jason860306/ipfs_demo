5b6140f2f308c79636e967d26083441c4f11ae61
jason860306 <jason860306@gmail.com>
2018-02-09 20:08:14 +0800

    commit ipfs_demo
    first commit

---
+++ main.go
@@ -0,0 +1,320 @@
+package main
+
+import (
+	"github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/namesys"
+	"github.com/ipfs/go-ipfs/repo/config"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
+	"github.com/jason860306/ipfs_demo/ipfs"
+	"github.com/mitchellh/go-homedir"
+	"github.com/op/go-logging"
+	"github.com/tyler-smith/go-bip39"
+	"path/filepath"
+
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+	"time"
+)
+
+const RepoVersion = "6"
+
+var log = logging.MustGetLogger("ipfs_demo")
+var ErrRepoExists = errors.New("IPFS configuration file exists. Reinitializing would overwrite your keys. Use -f to force overwrite.")
+
+var DefaultBootstrapAddresses = []string{
+	"/ip4/107.170.133.32/tcp/4001/ipfs/QmUZRGLhcKXF1JyuaHgKm23LvqcoMYwtb9jmh8CkP4og3K", // Le Marché Serpette
+	"/ip4/139.59.174.197/tcp/4001/ipfs/QmZfTbnpvPwxCjpCG3CXJ7pfexgkBZ2kgChAiRJrTK1HsM", // Brixton Village
+	"/ip4/139.59.6.222/tcp/4001/ipfs/QmRDcEDK9gSViAevCHiE6ghkaBCU7rTuQj4BDpmCzRvRYg",   // Johari
+	"/ip4/46.101.198.170/tcp/4001/ipfs/QmePWxsFT9wY3QuukgVDB7XZpqdKhrqJTHTXU7ECLDWJqX", // Duo Search
+}
+
+/* Returns the directory to store repo data in.
+   It depends on the OS and whether or not we are on testnet. */
+func GetRepoPath() (string, error) {
+	// Set default base path and directory name
+	path := "~"
+	directoryName := ".ipfs_demo"
+
+	// Join the path and directory name, then expand the home path
+	fullPath, err := homedir.Expand(filepath.Join(path, directoryName))
+	if err != nil {
+		return "", err
+	}
+
+	// Return the shortest lexical representation of the path
+	return filepath.Clean(fullPath), nil
+}
+
+func checkWriteable(dir string) error {
+	_, err := os.Stat(dir)
+	if err == nil {
+		// Directory exists, make sure we can write to it
+		testfile := path.Join(dir, "test")
+		fi, err := os.Create(testfile)
+		if err != nil {
+			if os.IsPermission(err) {
+				return fmt.Errorf("%s is not writeable by the current user", dir)
+			}
+			return fmt.Errorf("Unexpected error while checking writeablility of repo root: %s", err)
+		}
+		fi.Close()
+		return os.Remove(testfile)
+	}
+
+	if os.IsNotExist(err) {
+		// Directory does not exist, check that we can create it
+		return os.Mkdir(dir, 0775)
+	}
+
+	if os.IsPermission(err) {
+		return fmt.Errorf("Cannot write to %s, incorrect permissions", err)
+	}
+
+	return err
+}
+
+func datastoreConfig(repoRoot string) config.Datastore {
+	return config.Datastore{
+		StorageMax:         "10GB",
+		StorageGCWatermark: 90, // 90%
+		GCPeriod:           "1h",
+		BloomFilterSize:    0,
+		HashOnRead:         false,
+		Spec: map[string]interface{}{
+			"type": "mount",
+			"mounts": []interface{}{
+				map[string]interface{}{
+					"mountpoint": "/blocks",
+					"type":       "measure",
+					"prefix":     "flatfs.datastore",
+					"child": map[string]interface{}{
+						"type":      "flatfs",
+						"path":      "blocks",
+						"sync":      true,
+						"shardFunc": "/repo/flatfs/shard/v1/next-to-last/2",
+					},
+				},
+				map[string]interface{}{
+					"mountpoint": "/",
+					"type":       "measure",
+					"prefix":     "leveldb.datastore",
+					"child": map[string]interface{}{
+						"type":        "levelds",
+						"path":        "datastore",
+						"compression": "none",
+					},
+				},
+			},
+		},
+	}
+}
+
+func InitConfig(repoRoot string) (*config.Config, error) {
+	bootstrapPeers, err := config.ParseBootstrapPeers(DefaultBootstrapAddresses)
+	if err != nil {
+		return nil, err
+	}
+
+	datastore := datastoreConfig(repoRoot)
+
+	conf := &config.Config{
+
+		// Setup the node's default addresses.
+		// NOTE: two swarm listen addrs, one TCP, one UTP.
+		Addresses: config.Addresses{
+			Swarm: []string{
+				"/ip4/0.0.0.0/tcp/4001",
+				"/ip6/::/tcp/4001",
+				"/ip4/0.0.0.0/tcp/9005/ws",
+				"/ip6/::/tcp/9005/ws",
+			},
+			API:     "",
+			Gateway: "/ip4/127.0.0.1/tcp/4002",
+		},
+
+		Datastore: datastore,
+		Bootstrap: config.BootstrapPeerStrings(bootstrapPeers),
+		Discovery: config.Discovery{config.MDNS{
+			Enabled:  false,
+			Interval: 10,
+		}},
+
+		// Setup the node mount points
+		Mounts: config.Mounts{
+			IPFS: "/ipfs",
+			IPNS: "/ipns",
+		},
+
+		Ipns: config.Ipns{
+			ResolveCacheSize: 128,
+			RecordLifetime:   "7d",
+			RepublishPeriod:  "24h",
+		},
+
+		Gateway: config.Gateway{
+			RootRedirect: "",
+			Writable:     false,
+			PathPrefixes: []string{},
+		},
+	}
+
+	return conf, nil
+}
+
+func createMnemonic(newEntropy func(int) ([]byte, error), newMnemonic func([]byte) (string, error)) (string, error) {
+	entropy, err := newEntropy(128)
+	if err != nil {
+		return "", err
+	}
+	mnemonic, err := newMnemonic(entropy)
+	if err != nil {
+		return "", err
+	}
+	return mnemonic, nil
+}
+
+func addConfigExtensions(repoRoot string) error {
+	r, err := fsrepo.Open(repoRoot)
+	if err != nil { // NB: repo is owned by the node
+		return err
+	}
+	if err := r.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func initializeIpnsKeyspace(repoRoot string, privKeyBytes []byte) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	r, err := fsrepo.Open(repoRoot)
+	if err != nil { // NB: repo is owned by the node
+		return err
+	}
+	cfg, err := r.Config()
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	identity, err := ipfs.IdentityFromKey(privKeyBytes)
+	if err != nil {
+		return err
+	}
+
+	cfg.Identity = identity
+	nd, err := core.NewNode(ctx, &core.BuildCfg{Repo: r})
+	if err != nil {
+		return err
+	}
+	defer nd.Close()
+
+	err = nd.SetupOfflineRouting()
+	if err != nil {
+		return err
+	}
+
+	return namesys.InitializeKeyspace(ctx /*, nd.DAG*/, nd.Namesys, nd.Pinning, nd.PrivateKey)
+}
+
+func DoInit(repoRoot string, nBitsForKeypair int, password string, mnemonic string, creationDate time.Time) error {
+
+	if fsrepo.IsInitialized(repoRoot) {
+		return ErrRepoExists
+	}
+
+	if err := checkWriteable(repoRoot); err != nil {
+		return err
+	}
+
+	conf, err := InitConfig(repoRoot)
+	if err != nil {
+		return err
+	}
+
+	if mnemonic == "" {
+		mnemonic, err = createMnemonic(bip39.NewEntropy, bip39.NewMnemonic)
+		if err != nil {
+			return err
+		}
+	}
+	seed := bip39.NewSeed(mnemonic, "Secret Passphrase")
+	fmt.Printf("Generating Ed25519 keypair...")
+	identityKey, err := ipfs.IdentityKeyFromSeed(seed, nBitsForKeypair)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Done\n")
+
+	identity, err := ipfs.IdentityFromKey(identityKey)
+	if err != nil {
+		return err
+	}
+
+	log.Infof("Initializing OpenBazaar node at %s\n", repoRoot)
+	if err := fsrepo.Init(repoRoot, conf); err != nil {
+		return err
+	}
+	conf.Identity = identity
+
+	if err := addConfigExtensions(repoRoot); err != nil {
+		return err
+	}
+
+	f, err := os.Create(path.Join(repoRoot, "repover"))
+	if err != nil {
+		return err
+	}
+	_, werr := f.Write([]byte(RepoVersion))
+	if werr != nil {
+		return werr
+	}
+	f.Close()
+
+	return initializeIpnsKeyspace(repoRoot, identityKey)
+}
+
+func InitializeRepo(dataDir, password, mnemonic string, creationDate time.Time) error {
+	// Initialize the IPFS repo if it does not already exist
+	return DoInit(dataDir, 4096, password, mnemonic, creationDate)
+}
+
+func main() {
+	// Set repo path
+	repoPath, err := GetRepoPath()
+	if err != nil {
+		os.Exit(1)
+	}
+	fmt.Println(repoPath)
+
+	passwd := strings.Replace("123456", "'", "''", -1)
+	Mnemonic := "ipfs_demo"
+	creationDate := time.Now()
+
+	err = InitializeRepo(repoPath, passwd, Mnemonic, creationDate)
+	if err == ErrRepoExists {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Force overwriting the db will destroy your existing keys and history. Are you really, really sure you want to continue? (y/n): ")
+		resp, _ := reader.ReadString('\n')
+		if strings.ToLower(resp) == "y\n" || strings.ToLower(resp) == "yes\n" || strings.ToLower(resp)[:1] == "y" {
+			os.RemoveAll(repoPath)
+			err = InitializeRepo(repoPath, passwd, Mnemonic, creationDate)
+			if err != nil {
+				os.Exit(1)
+			}
+			fmt.Printf("OpenBazaar repo initialized at %s\n", repoPath)
+			os.Exit(1)
+		} else {
+			os.Exit(1)
+		}
+	} else if err != nil {
+		os.Exit(1)
+	}
+	fmt.Printf("OpenBazaar repo initialized at %s\n", repoPath)
+	os.Exit(1)
+}
