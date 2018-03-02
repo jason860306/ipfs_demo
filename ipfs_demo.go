c9e3735570a4b1dbeec4e763faf3cf639f6b3ea2
root <root@centos7-do.localdomain>
2018-03-02 08:46:29 +0000

    update supernode's peerid

--- ipfs_demo.go
+++ ipfs_demo.go
@@ -48,7 +48,7 @@ var DefaultBootstrapAddresses = []string{
 	//"/ip4/139.59.174.197/tcp/4001/ipfs/QmZfTbnpvPwxCjpCG3CXJ7pfexgkBZ2kgChAiRJrTK1HsM", // Brixton Village
 	//"/ip4/139.59.6.222/tcp/4001/ipfs/QmRDcEDK9gSViAevCHiE6ghkaBCU7rTuQj4BDpmCzRvRYg",   // Johari
 	//"/ip4/46.101.198.170/tcp/4001/ipfs/QmePWxsFT9wY3QuukgVDB7XZpqdKhrqJTHTXU7ECLDWJqX", // Duo Search
-	"/ip4/138.197.232.22/tcp/4001/ipfs/QmZjmQH4e7opwmeFc23vUZ4nwuPw1oJgFKSpJoAJgrpQiy", // szj0306
+	"/ip4/138.197.232.22/tcp/4001/ipfs/QmXC5YrfyLtwcx3Gh9bN1Dx6K2o2PAhCp9LV7Cy2J3gNdL", // szj0306
 }
 
 /* Returns the directory to store repo data in.
